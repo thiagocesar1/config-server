c261ce73e10463cd685d2a6ddcddc1d582374c3d
Thiago Alvares <thiago.cesaralvares@gmail.com>
2022-05-31 20:11:27 -0300

    Implementação inicial do config server

---
+++ src/test/java/br/com/micro/configserver/Applicaiton.java
@@ -0,0 +1,13 @@
+package br.com.micro.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Applicaiton {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
